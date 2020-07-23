8c49835018f2ebf8f7267baa56055b7f88f9112d
nori122 <norito.sasaki.jp@gmail.com>
2020-07-24 01:41:31 +0900

    dangerCnt、dangerLimitの変数を追加

--- 0723_rendav3_Nori/Assets/Scripts/DangerCounter.cs
+++ 0723_rendav3_Nori/Assets/Scripts/DangerCounter.cs
@@ -7,16 +7,34 @@ using UnityEngine.SceneManagement;
 public class DangerCounter : MonoBehaviour
 {
     // カウント用変数
-    public int dangerCnt;
+    public int dangerCnt = 0;
+    public int dangerLimit = 20;
+
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        // マウスがクリックされたら
+        if (Input.GetMouseButtonDown(0))
+        {
+            // カウントを減らす
+            dangerCnt++;
+        }
 
+        //カウントをTextコンポーネントへ 
+        this.GetComponent<Text>().text = "保水力は" + dangerCnt + "%です";
+
+        // もしdangerCntが20以上になったら
+        if (dangerCnt == dangerLimit)
+        {
+            // カウントを0に戻す
+            //カウントをTextコンポーネントへ
+            // this.GetComponent<Text>().text = "結果は" + result + "秒でした";
+            SceneManager.LoadScene("ResultScene");
+        }
     }
 }
