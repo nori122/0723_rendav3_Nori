7a4d3abda5d54ed8e69dabb48cfa1c47e6ae51f6
nori122 <norito.sasaki.jp@gmail.com>
2020-07-24 06:14:20 +0900

    雨のシーンをゲームシーンに無理やり追加

---
+++ 0723_renda_ver5_Nori/Assets/Scripts/Result.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Result : TimeCounter
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Text>().text = "結果は" + countup.ToString("f2") + "秒でした";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ 0723_renda_ver5_Nori/Assets/Scripts/TimeCounter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimeCounter : MonoBehaviour
+{
+    //カウントアップ
+    public static float countup = 0.0f;
+
+    //時間を表示するText型の変数
+    public Text timeText;
+
+    void Start()
+    {
+        countup = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //時間をカウントする
+        countup += Time.deltaTime;
+
+        //時間を表示する
+        timeText.text = countup.ToString("f2") + "秒";
+    }
+}
